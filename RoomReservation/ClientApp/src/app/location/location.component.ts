e4c41730c8e2d2cd5ad446ecb9ead98a8d3b339a
Josh Oldes <jcoldes@outlook.com>
2022-06-14 16:12:44 -0700

    Setup each initial page.

---
+++ RoomReservation/ClientApp/src/app/location/location.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  templateUrl: "location.component.html",
+  styleUrls: ["location.component.css"]
+})
+export class LocationComponent { }
